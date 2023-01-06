7e82de8f1d523202a2602b96658f13c37abd7cf5
Muqeeth99 <85898448+Muqeeth99@users.noreply.github.com>
2023-01-06 19:18:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template, send_file
 import io
+import cv2
 import torch
 from torch import autocast
+from PIL import Image
 from diffusers import StableDiffusionPipeline
 
-app = Flask(__name__, template_folder='template',static_folder='/Users/mac/Documents/SD/Stable-diffuser-main/Static')
+app = Flask(__name__, template_folder='template',static_folder='./Static')
 
 assert torch.cuda.is_available()
 gc.collect()
@@ -19,7 +21,16 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float
 pipe = pipe.to(device)
 pipe.enable_attention_slicing()
 
+super_res = cv2.dnn_superres.DnnSuperResImpl_create()
 
+def image_to_byte_array(image: Image) -> bytes:
+  imgByteArr = io.BytesIO()
+  image.save(imgByteArr, format=image.format)
+  imgByteArr = imgByteArr.getvalue()
+  return imgByteArr
+
+
+@app.route('/generate/<string:prompt>')
 def run_inference(prompt):
     gc.collect()
     torch.cuda.empty_cache()
@@ -28,22 +39,19 @@ def run_inference(prompt):
     texts = [ text ] * num_images
     with autocast(device):
         image = pipe(texts, guidance_scale=7.5, num_inference_steps=10).images[0] 
-    image.save("./Static/gen_image.jpeg")
+    image.save("./Static/gen_image.jpeg", "JPEG")
+    
+    img = cv2.imread('./Static/gen_image.jpeg')
+    super_res.readModel('LapSRN_x8.pb')
+    super_res.setModel('lapsrn',8)
+    lapsrn_image = super_res.upsample(img)
+    cv2.imwrite(r'./Static/Lapsen.jpeg',lapsrn_image)
 
-    return
+    img = Image.open('./Static/Lapsen.jpeg', mode='r')
+    mime = Image.MIME[img.format]
+    img = image_to_byte_array(img)
+    return send_file(io.BytesIO(img), mimetype=mime)
 
-
-@app.route("/")
-def myapp():
-    return render_template("index.html", title="AI txt2img")
-
-
-@app.route('/results', methods= ["POST"])
-def submit():
-    if request.method == 'POST':
-        prompt = request.form.get("Prompt")
-        img_data = run_inference(prompt)
-    return render_template("results.html",n=img_data)
 
 if __name__ == '__main__':
     app.run()
